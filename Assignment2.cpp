9d1431f8cc718f3cc9a963536df9222c93e9390d
HarleeScordo <113852333+HarleeScordo@users.noreply.github.com>
2023-09-07 00:11:43 -0400

    Add files via upload

---
+++ Assignment2.cpp
@@ -0,0 +1,108 @@
+/// Assignment 2
+//
+///#include <iostream>
+///using namespace std;
+////int main()
+///{
+///	int temp, velocity;
+///	cout << "Enter the temparature: ";
+///	cin >> temp;
+///velocity = 331.3 + temp * 0.61;
+///	cout << "At " << temp << " degrees celcius the velocity of the sound is " << velocity << " m/s" << endl;
+///	return 0;
+//}
+/// 
+
+///Part 2
+/// 2
+/// 6
+/// 22
+/// -66
+/// 16
+
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main()
+{
+
+	string inname, name, food, number, adjective, color, animal;
+	string word;
+
+		char space;
+
+
+	int num;
+
+	cout << "Enter your instructors name." << endl;
+	cin >> inname;
+
+	cout << "Enter your name." << endl;
+	cin >> name;
+
+	cout << "Enter a type of food." << endl;
+	cin >> food;
+
+	cout << "Enter a number between 100 and 120." << endl;
+	cin >> number;
+
+	cout << "Enter an adjective." << endl;
+	cin >> adjective;
+
+	cout << "Enter a color." << endl;
+	cin >> color;
+
+	cout << "FinalLy, enter an animal." << endl;
+	cin >> color;
+
+	cout << "    " << endl;
+	cout << "    " << endl;
+
+
+
+	cout << "Dear instructor "; 
+	cout << inname;
+
+	cout << "   " << endl;
+	cout << "    " << endl;
+
+	cout << "I am sorry that I am unable to turn in my homework at this time.";
+
+	cout << " First, I ate a rotten ";
+	cout << food;
+
+	cout << ", which made me turn ";
+	cout << color;
+
+	cout << " and extremely ill. I came down with a fever of ";
+	cout << number;
+
+	cout << ". Next my ";
+	cout << adjective;
+
+	cout << " pet ";
+	cout << animal;
+
+	cout << " must have smelled the remains of the :";
+	cout << food;
+
+	cout << " on my homework, because he ate it.";
+	cout << " I am currently rewritting my homework and hope you will accept it here.";
+
+	cout << "    " << endl;
+	cout << "     " << endl;
+
+	cout << "Sincerely, ";
+	cout << name;
+
+	cout << "     " << endl;
+	cout << "    " << endl;
+
+
+
+	return 0;
+}
