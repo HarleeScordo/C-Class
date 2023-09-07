a7b2b5e070dd8da4267ac7254f6595d0ed5891e7
HarleeScordo <113852333+HarleeScordo@users.noreply.github.com>
2023-09-06 23:46:28 -0400

    Update Lab6.cpp

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,50 +1,54 @@
- // 
+// 
  // This program determines the fee for a cat or dog pet tag. 
  // It uses nested if/else statements. 
  // PUT YOUR NAME HERE. 
- #include <iostream>
- #include <string>
- using namespace std;
+#include <iostream>
+#include <string>
+using namespace std;
 
- int main()
- {
- 
-  string pet; // "cat" 10 { or "dog"
- char spayed; // 'y' or 'n'
+int main()
+{
 
- // Get pet type and spaying information
- cout << "Enter the pet type (cat or dog): ";
- cin >> pet;
- if(pet == "cat" || pet =="dog")
+	string pet; // "cat" 10 { or "dog"
+	char spayed; // 'y' or 'n'
 
- cout << "Has the pet been spayed or neutered (y/n)? ";
- cin >> spayed;
- 
- // Determine the pet tag fee 
- if (pet == "cat")
- { if (spayed == 'y' || spayed == 'Y')
-	  cout << "Fee is $4.00 \n";
- else
- cout << "Fee is $8.00 \n";
+	// Get pet type and spaying information
+	cout << "Enter the pet type (cat or dog): ";
+	cin >> pet;
+	if (pet == "cat" || pet == "dog")
+	{
+		cout << "Has the pet been spayed or neutered (y/n)? ";
+		cin >> spayed;
 
- }
+		// Determine the pet tag fee 
+		if (pet == "cat")
+		{
+			if (spayed == 'y' || spayed == 'Y')
+				cout << "Fee is $4.00 \n";
+			else
+				cout << "Fee is $8.00 \n";
 
- else if (pet == "dog")
- { if (spayed == 'y' || spayed == 'Y')
- cout << "Fee is $6.00 \n";
- else
- cout << "Fee is $12.00 \n";
- }
- else
- cout << "Only cats and dogs need pet tags. \n";
+		}
 
- return 0;
- }
+		else if (pet == "dog")
+		{
+			if (spayed == 'y' || spayed == 'Y')
+				cout << "Fee is $6.00 \n";
+			else
+				cout << "Fee is $12.00 \n";
+		}
 
- // 1 - $4
- // 2 - $8
- // 3 - $8
- // 4 - $6
- // 5 - $12
- // 6 - $12
- // 7 - Only cats and dogs need pet tags. 
+	}
+	else
+		cout << "Only cats and dogs need pet tags.";
+
+	return 0;
+}
+
+// 1 - $4
+// 2 - $8
+// 3 - $8
+// 4 - $6
+// 5 - $12
+// 6 - $12
+// 7 - Only cats and dogs need pet tags. 
